61bc46f2f51e42fede198a2baf210482009cd6e0
LinVince <vincejim91126@gmail.com>
2024-01-05 09:13:23 +0800

    relevance as dependencies

--- app.py
+++ app.py
@@ -67,14 +67,14 @@ for i in raw_data[1:]:
     new_data['relevance'] = gen_ran_relevant(60, 90)
     parent_co = loc_topic_co(new_data['topic'],new_data_json['nodes'])
     if parent_co:
-      (new_data['longitude'], new_data['latitude']) = gen_co(parent_co, 100, new_data['relevance'])
+      (new_data['longitude'], new_data['latitude']) = gen_co(parent_co, 10, new_data['relevance'])
     
 
   elif new_data['type'] == 'sub-subtopic':
     new_data['relevance'] = gen_ran_relevant(1, 50)
     parent_co = loc_subtopic_co(new_data['subtopic'],new_data_json['nodes'])
     if parent_co:
-      (new_data['longitude'], new_data['latitude']) = gen_co(parent_co, 1, new_data['relevance'])
+      (new_data['longitude'], new_data['latitude']) = gen_co(parent_co, 5, new_data['relevance'])
 
   new_data_json['nodes'].append(new_data)
 
